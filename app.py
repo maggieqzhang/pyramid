783ba85fd6320744be0f179eda3e1141c9b73c79
Neo-Techies <mbcse50@gmail.com>
2020-08-31 21:52:29 +0530

    Added profile and orders Api endpoint

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+from functools import wraps
+import json
+from bson import json_util
 import bcrypt
 
 app = Flask(__name__)
-
-
-app.config['MONGO_URI'] = ''
-
+app.secret_key = 'pyramid'
+app.config['MONGO_URI'] = 'mongodb+srv://pyramid:pyramid@openwater.chp4s.mongodb.net/pyramid?retryWrites=true&w=majority'
 mongo = PyMongo(app)
-print(mongo)
+
+
+def checkLoggedIn():
+    def check(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            print(session['username'])
+            print(session)
+            if 'username' in session:
+                return func(*args, **kwargs)
+            else:
+                return {"Error":"Please Login"}
+        return inner
+    return check               
+
+
+
+
 @app.route('/')
 def index():
     if 'username' in session:
         return 'You are logged in as ' + session['username']
 
-    return {'DONE':'HEllo'}
+    return {'Hi':'Hi'}
 
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-    login_user = users.find_one({'name' : request.form['username']})
+    login_user = users.find_one({'username' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return redirect(url_for('index'))
 
@@ -32,12 +50,12 @@ def login():
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
+        existing_user = users.find_one({'username' : request.form['username']})
         print(existing_user)
 
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            users.insert({'username' : request.form['username'], 'password' : hashpass, 'orders':[]})
             session['username'] = request.form['username']
             return redirect(url_for('index'))
         
@@ -45,6 +63,19 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/profile', methods=['GET'])
+@checkLoggedIn()
+def profile():
+    user = mongo.db.users.find_one({'username' : session['username']})
+    user.pop('_id')
+    user.pop('password')
+    return json_util.dumps({'user':user})
+    
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.secret_key = 'pyramid'
     app.run(debug=True)
