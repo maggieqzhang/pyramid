a9937d557ebbc788096e10ee3f984802dac9e7fc
maggieqzhang <maggieqz@mit.edu>
2020-08-31 17:06:40 -0700

    created restaurant collection, registration, login, and order form

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 from bson import json_util
 import bcrypt
 import dns
+from datetime import date
 from forms import *
 
 app = Flask(__name__)
@@ -26,26 +27,32 @@ def checkLoggedIn():
         return inner
     return check     
           
-'''
-@app.before_request
-def before_request():
-    if "username" in session:
-        users = mongo.db.users
-        login_user = users.find_one({'name' : request.form['username']})
-        g.login_user = login_user
-'''
-
 @app.route('/', methods = ['POST', 'GET'])
-def index():
-    form = SearchForm()
-    if form.is_submitted():
-        result = request.form
-        return result
+def index(): #need to check if it's a restaurant or if it's a user who is trying to buy 
+    if request.method == 'POST':
+        if 'type' not in session or session['type'] == 'user': #person just entered our website and wants to search w/out having logged in
+            search_query = {'food': request.form['cuisine'], 'time': request.form['time'] }
+            #[URGENT]figure out how to pass this over to the next search query step
+            
+            
+        elif session['type'] == 'restaurant': 
+            #find which restaurant it belongs to and then add that in their order form 
+            restaurants = mongo.db.restaurants
+            restaurantName = restaurants.update(
+                    {'address.streetAddress': session['address'] }, {$addToSet:{'orders': {
+                    'date': date.today(),
+                    'time': request.form['time'],
+                    'maxOrders': request.form['maxOrders'],
+                    'ordersFulfilled': 0 
+                    }}}
+                )
+            return redirect(url_for('index'))
+            
         
-    if 'username' in session:
+    if 'username' in session: 
         return 'You are logged in as ' + session['username']
 
-    return render_template('forms/search.html', form = form)
+    return {1:1} 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -55,26 +62,33 @@ def login():
     if login_user:
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
+            session['type'] = 'user' 
             return redirect(url_for('index'))
 
     return 'Invalid username/password combination'
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    session.pop('username', None)
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'username' : request.form['username']})
-        print(existing_user)
 
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'username' : request.form['username'], 'password' : hashpass, 'orders':[]})
+            users.insert(
+                    {
+                    'username' : request.form['username'], 
+                    'password' : hashpass, 
+                    'email': request.form['email'], 
+                    'orders':[]
+                    }
+                    )
             session['username'] = request.form['username']
             return redirect(url_for('index'))
-        
         return 'That username already exists!'
-
     return render_template('forms/register.html')
+
 
 @app.route('/profile', methods=['GET'])
 @checkLoggedIn()
@@ -83,6 +97,53 @@ def profile():
     user.pop('_id')
     user.pop('password')
     return json_util.dumps({'user':user})
+
+
+@app.route('/restaurantregister', methods = ['GET', 'POST'])
+def restaurantRegister():
+    session.pop('username', None)
+    if request.method == 'POST':
+        restaurants = mongo.db.restaurants
+        existing_restaurant = restaurants.find_one({'restaurant' : request.form['restaurant']})
+        if existing_restaurant is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            restaurants.insert(
+                    {
+                    'username' : request.form['username'], 
+                    'password' : hashpass, 
+                    'email': request.form['email'], 
+                    'address':{
+                    'restaurant': request.form['restaurant'], 
+                    'streetAddress': request.form['streetAddress'],
+                    'city': request.form['city'],
+                    'state': request.form['state'],
+                    'zipcode': request.form['zipcode']},
+                    'orders':[]
+                     }
+                    )
+            session['username'] = request.form['restaurant']
+            session['type'] = 'restaurant'
+            session['address'] = request.form['streetAddress']
+            return redirect(url_for('index'))
+        
+        return 'We already have your restaurant in our system!'
+    
+    return render_template('forms/register.html')
+
+    
+@app.route('/restaurantlogin', methods = ['GET', 'POST'])
+def restaurantLogin():
+    restaurants = mongo.db.restaurants
+    login_user = restaurants.find_one({'username' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            session['type'] = 'restaurant' 
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,16 +1,13 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, SubmitField, DateTimeField
-from wtforms.validators import DataRequired, EqualTo, Length
-
-# Set your classes here.
-
+from wtforms import TextField, PasswordField, SubmitField, DateTimeField, IntegerField
+from wtforms.validators import DataRequired, EqualTo, Length, Email, NumberRange
 
 class RegisterForm(Form):
-    name = TextField(
+    username = TextField(
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
     )
     email = TextField(
-        'Email', validators=[DataRequired(), Length(min=6, max=40)]
+        'Email', validators=[DataRequired(), Email(message = "not a valid email address"), Length(min=6, max=40)]
     )
     password = PasswordField(
         'Password', validators=[DataRequired(), Length(min=6, max=40)]
@@ -23,8 +20,8 @@ class RegisterForm(Form):
 
 
 class LoginForm(Form):
-    name = TextField('Username', [DataRequired()])
-    password = PasswordField('Password', [DataRequired()])
+    username = TextField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 
 class ForgotForm(Form):
@@ -32,7 +29,41 @@ class ForgotForm(Form):
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
 
+
 class SearchForm(Form):
     cuisine = TextField('Cuisine or Restaurant Name')
     time = DateTimeField('Pickup Time')
     submit = SubmitField('Search')
+    
+    
+class RestaurantRegister(Form):
+    restaurant = TextField('Restaurant Name', [DataRequired()])
+    streetAddress = TextField('Street Address', validators=[DataRequired()])
+    city = TextField('City', validators=[DataRequired()])
+    state = TextField('State', validators= [DataRequired()])
+    zipcode = TextField('Zip Code', validators= [DataRequired()])
+    username = TextField(
+        'Username', validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = TextField(
+        'Email', validators=[DataRequired(),Email(message = "not a valid email address"), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators= [DataRequired(),
+        EqualTo('password', message='Passwords must match')]
+    )
+    
+    
+class RestaurantLogin(Form):
+    username = TextField('Username', validators= [DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    
+    
+class RestaurantOrders(Form):
+    location = TextField('Drop Off Location', validators=[DataRequired()])
+    time = DateTimeField('Drop Off Time', validators=[DataRequired()])
+    maxOrders = IntegerField('Max Orders', validators=[DataRequired(), NumberRange(min = 0, message = "Must allow at least 1 order!")])
